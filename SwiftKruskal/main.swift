51254fad4ef4bae228311902c26cc7bfedc95c98
sossam <heart_b_e@naver.com>
2021-02-03 16:39:27 +0900

    ✨ 크루스칼 함수 추가

--- SwiftKruskal/main.swift
+++ SwiftKruskal/main.swift
@@ -7,3 +7,47 @@
 
 import Foundation
 
+typealias edge = (Int, String, String)
+
+func kruskal(vertices: [String], edges: [edge]) -> [edge] {
+    var mst: [edge] = []
+    
+    var edges = edges.sorted { $0.0 < $1.0 }
+    var rank: [String: Int] = [:]
+    var parent: [String: String] = [:]
+    
+    for vertice in vertices {
+        rank.updateValue(0, forKey: vertice)
+        parent.updateValue(vertice, forKey: vertice)
+    }
+    
+    func find(_ node: String) -> String {
+        if node != parent[node]! {               // 루트 노드 찾아야만 재귀 탈출
+            parent[node] = find(parent[node]!)
+        }
+        return parent[node]!
+    }
+    
+    func union(_ nodeV: String, _ nodeU: String) {
+        let rankV = find(nodeV)
+        let rankU = find(nodeU)
+        
+        if rankV > rankU {
+            parent[rankU] = rankV
+        } else {
+            parent[nodeV] = nodeU
+            if rankV == rankU {
+                rank[nodeU]! += 1
+            }
+        }
+    }
+    
+    while mst.count < (vertices.count - 1) {
+        let node = edges.removeFirst()
+        if find(node.1) != find(node.2) {
+            union(node.1, node.2)
+            mst.append(node)
+        }
+    }
+    return mst
+}
